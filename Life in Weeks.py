bc21422e681da859fb84c1c8aad3fcf6627f25c0
rutera <4562455+rutera@users.noreply.github.com>
2022-11-26 19:24:12 +0200

    Create Life in Weeks.py

---
+++ Life in Weeks.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+year = int(90) - int(age)
+x = year * 365
+y = year * 52
+z = year * 12
+
+print(f"You have {x} days, {y} weeks, and {z} months left.")
